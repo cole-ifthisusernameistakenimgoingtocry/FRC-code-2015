3ccc21caa796a774d94f01badff667216e8d807b
Cole Carrow <colec518@gmail.com>
2015-02-15 09:19:52 -0800

    fixed an import

--- gravityLoop/src/org/usfirst/frc/team5004/robot/subsystems/Fork.java
+++ gravityLoop/src/org/usfirst/frc/team5004/robot/subsystems/Fork.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team5004.robot.subsystems;
 
 import org.usfirst.frc.team5004.robot.RobotMap;
-import org.usfirst.frc.team5004.robot.commands.ForkCommand;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -15,7 +14,7 @@ public class Fork extends Subsystem {
 	}
 	
 	protected void initDefaultCommand() {
-		setDefaultCommand(new ForkCommand());
+	
 	}
 
 }
